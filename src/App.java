ce723713241f0fc814d224f443392ef0a91a261b
virajabhang411 <viraj@wigzo.com>
2023-05-08 18:01:10 +0530

    broadcas1t

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.print.attribute.standard.DateTimeAtCreation;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -28,7 +33,7 @@ driver.findElement(By.xpath("/html/body/div/div/div[4]/section/form/div/div/div/
 Thread.sleep(2000);
 driver.findElement(By.xpath("/html/body/div/div/div[4]/section/div/button")).click();
 Thread.sleep(2000);
-driver.findElement(By.xpath("/html/body/div/div/div[4]/section/div/div[1]/form/div/div/div/div[1]/div[1]/div/div/div/input")).sendKeys("Automation test Broadcast");
+driver.findElement(By.xpath("/html/body/div/div/div[4]/section/div/div[1]/form/div/div/div/div[1]/div[1]/div/div/div/input")).sendKeys("Automation test Broadcast" +  LocalDate.now());
 Thread.sleep(2000);
 driver.findElement(By.id("mui-component-select-selectedTemp")).click();
 Thread.sleep(2000);
