e761df4b618463d6a6dde515f43cd43616fc645c
virajabhang411 <viraj@wigzo.com>
2023-05-08 15:27:57 +0530

    srtoken

--- src/App.java
+++ src/App.java
@@ -11,26 +11,23 @@ System.setProperty("webdriver.chrome.driver", "C:/Users/abhan/Downloads/chromedr
 
 WebDriver driver = new ChromeDriver();
 
-driver.get("https://app.shiprocket.in");
+driver.get("https://sr-convert.shiprocket.in/lite/auth/shiprocket/login/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMxMzMxMDksImlzcyI6Imh0dHBzOi8vYXBpdjIuc2hpcHJvY2tldC5pbi92MS9hdXRoL2xvZ2luIiwiaWF0IjoxNjgzNTM4ODQyLCJleHAiOjE2ODQ0MDI4NDIsIm5iZiI6MTY4MzUzODg0MiwianRpIjoiWERrb3lyVjRQQzlaVXNoRCJ9.hOcPNe4JNZF1hyrX_FvlGR6AnMhgCLXM-Mzb02y31jA");
 
-driver.manage().window().maximize();
+//driver.manage().window().maximize();
 
-driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div[2]/div[1]/form/div[1]/div[1]/input")).sendKeys("viraj+02@wigzo.com");
-
-//driver.wait(2000);
-driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div[2]/div[1]/form/div[1]/div[2]/input")).sendKeys("Viraj@123");
-
-driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div[2]/div[1]/form/div[3]/div/button")).click();
-
-driver.findElement(By.className("/html/body/app-root/app-layout/div[1]/app-navbar/sr-sidebar/app-header/mat-toolbar/button[3]/span[1]")).click();
-
+//driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div[2]/div[1]/form/div[1]/div[1]/input")).sendKeys("viraj+02@wigzo.com");
 
 //driver.wait(2000);
+//driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div[2]/div[1]/form/div[1]/div[2]/input")).sendKeys("Viraj@123");
 
-driver.close();
- System.out.println("login");
+//driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div[2]/div[1]/form/div[3]/div/button")).click();
+
+//driver.findElement(By.className("/html/body/app-root/app-layout/div[1]/app-navbar/sr-sidebar/app-header/mat-toolbar/button[3]/span[1]")).click();
+
 
 
 
 }
 }
+
+
